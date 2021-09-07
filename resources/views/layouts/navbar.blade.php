11209d490e6ab0ddcc87a79764fd5baf598d8cb1
Putthapong Putthapongwong <aitooy2123@gmail.com>
2021-09-08 00:25:27 +0700

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Auth;
 ?>
 
 <!-- Navbar -->
-<nav class="main-header navbar navbar-expand  navbar-white navbar-light">
+<nav class="main-header navbar navbar-expand  navbar-white navbar-light fixed-top">
   <!-- Left navbar links -->
   <ul class="navbar-nav">
     <li class="nav-item">
